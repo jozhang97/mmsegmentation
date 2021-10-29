eef0a4589159beb2a061432c4869267f97316a25
JEFFREY ZHANG <jozhang@berkeley.edu>
2021-10-29 16:45:43 -0500

    update __init__ for pipeline

--- mmseg/datasets/pipelines/__init__.py
+++ mmseg/datasets/pipelines/__init__.py
@@ -7,11 +7,14 @@ from .test_time_aug import MultiScaleFlipAug
 from .transforms import (CLAHE, AdjustGamma, Normalize, Pad,
                          PhotoMetricDistortion, RandomCrop, RandomFlip,
                          RandomRotate, Rerange, Resize, RGB2Gray, SegRescale)
+from .box2seg_transform import Crop_with_Mask, RandomGamma
+from .loading_uvo import LoadImageWithBBox
 
 __all__ = [
     'Compose', 'to_tensor', 'ToTensor', 'ImageToTensor', 'ToDataContainer',
     'Transpose', 'Collect', 'LoadAnnotations', 'LoadImageFromFile',
     'MultiScaleFlipAug', 'Resize', 'RandomFlip', 'Pad', 'RandomCrop',
     'Normalize', 'SegRescale', 'PhotoMetricDistortion', 'RandomRotate',
-    'AdjustGamma', 'CLAHE', 'Rerange', 'RGB2Gray'
+    'AdjustGamma', 'CLAHE', 'Rerange', 'RGB2Gray',
+    'Crop_with_Mask', 'RandomGamma', 'LoadImageWithBBox',
 ]
